92bad9220c1a4f8a939508ae08c1ffbdca81f4a2
Adam  Shappy <ajs11300@gmail.com>
2020-01-05 18:35:03 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,46 @@
-# Add your code here
+# # Add your code here
+# def say_hello(name)
+#   "Hi #{name}!"
+# end
+
+# puts "Enter your name"
+# users_name = gets.strip
+
+# puts say_hello(users_name)
+
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+def help 
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  number_or_song = gets.strip
+  if number_or_song =~ /\d/ 
+    integer = number_or_song.to_i
+    puts "Playing #{songs[integer-1]}"
+  else 
+    puts "Playing #{number_or_song}"
+  end
+end
+
+def list (songs)
+   songs.each_with_index {
+     |el, i| puts "#{i}. #{el}"
+   }
+end
